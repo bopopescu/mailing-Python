b344abc7566da8090d92332d6331dffd7b947ccd
Marek Baranski <marek.baranski@capgemini.com>
2019-03-04 12:14:25 +0100

    add CC and BCC to email

--- main.py
+++ main.py
@@ -8,18 +8,25 @@ from email import encoders
 from getpass import getpass
 
 send_from = 'emilialechart@wp.pl'
-send_to = ['marek.baranski@interia.pl']
+send_to = ['maro.baranski@gmail.com']
+send_cc = ['marek.baranski@capgemini.com']
+send_bcc = ['marek.baranski@interia.pl']
 subject = 'test'
 files = []
 password = getpass("password: ")
 
 
-def send_mail(send_from, send_to, subject, password, files=[], server="localhost"):
+def send_mail(send_from, send_to, send_cc, send_bcc, subject, password, files=[], server="localhost"):
     assert type(send_to) == list
+    assert type(send_cc) == list
+    assert type(send_bcc) == list
     assert type(files) == list
-    msg = MIMEMultipart()
+
+    msg = MIMEMultipart('alternative')
     msg['From'] = send_from
     msg['To'] = COMMASPACE.join(send_to)
+    msg['Cc'] = COMMASPACE.join(send_cc)
+    msg['Bcc'] = COMMASPACE.join(send_bcc)
     msg['Date'] = formatdate(localtime=True)
     msg['Subject'] = subject
 
@@ -36,8 +43,8 @@ def send_mail(send_from, send_to, subject, password, files=[], server="localhost
 
     server.starttls()
     server.login(send_from, password)
-    server.sendmail(send_from, send_to, msg.as_string())
+    server.sendmail(send_from, send_to + send_cc + send_bcc, msg.as_string())
     server.close()
 
 
-send_mail(send_from, send_to, subject, password, files, server=smtplib.SMTP('smtp.wp.pl', 587))
+send_mail(send_from, send_to, send_cc, send_bcc, subject, password, files, server=smtplib.SMTP('smtp.wp.pl', 587))
